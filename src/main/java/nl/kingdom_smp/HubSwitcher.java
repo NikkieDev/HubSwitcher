c8335af3033129c20e59bfb008549e011502818a
Nikkie Schaad <nikkie@schaad.zip>
2026-08-08 17:21:30 +0200

    Initial commit

---
+++ src/main/java/nl/kingdom_smp/HubSwitcher.java
@@ -0,0 +1,10 @@
+package nl.kingdom_smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HubSwitcher extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("HubSwitcher v0.1 enabled, thank you");
+	}
+}
